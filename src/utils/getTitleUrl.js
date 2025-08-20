ca0d58b9b6abdcf0b32c1c585ce686f008bd47a8
Yuuzi261 <lokasu0531@gmail.com>
2025-08-21 00:56:49 +0800

    fix: use webp for mobile title image

--- src/utils/getTitleUrl.js
+++ src/utils/getTitleUrl.js
@@ -2,6 +2,6 @@ import { getAssetsFile } from './getAssetsFile'
 
 export function getTitleUrl(locale, isMobile) {
   if (locale === null || locale === undefined) return ''
-  if (isMobile) return getAssetsFile(`img/title/mobile.png`)
+  if (isMobile) return getAssetsFile(`img/title/mobile.webp`)
   return getAssetsFile(`img/title/${locale}.webp`)
 }
