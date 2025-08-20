2e1a5d20a42752cfc3cc4f984c59d08875ff0964
Yuuzi261 <lokasu0531@gmail.com>
2025-08-20 23:38:24 +0800

    refactor: replace png with webp

--- src/utils/getTitleUrl.js
+++ src/utils/getTitleUrl.js
@@ -3,5 +3,5 @@ import { getAssetsFile } from './getAssetsFile'
 export function getTitleUrl(locale, isMobile) {
   if (locale === null || locale === undefined) return ''
   if (isMobile) return getAssetsFile(`img/title/mobile.png`)
-  return getAssetsFile(`img/title/${locale}.png`)
+  return getAssetsFile(`img/title/${locale}.webp`)
 }
