d9793766b5a1e2000545629f9c90f23d6f6f729f
Yuuzi261 <lokasu0531@gmail.com>
2025-11-17 08:13:11 +0800

    chore: format code

--- src/store/student.js
+++ src/store/student.js
@@ -3,34 +3,38 @@ import { ref, computed } from 'vue'
 import { useStudentData } from '@/utils/fetchStudentData'
 import { useShareableSelection } from '@/composables/useShareableSelection'
 
-export const useStudentStore = defineStore('student', () => {
-  const { data: studentsData } = useStudentData()
+export const useStudentStore = defineStore(
+  'student',
+  () => {
+    const { data: studentsData } = useStudentData()
 
-  const selectedStudentIds = ref([])
+    const selectedStudentIds = ref([])
 
-  const selectedStudents = computed(() => {
-    if (!studentsData.value) return []
-    return selectedStudentIds.value.map((id) => studentsData.value.find((s) => s.id === id)).filter(Boolean)
-  })
+    const selectedStudents = computed(() => {
+      if (!studentsData.value) return []
+      return selectedStudentIds.value.map((id) => studentsData.value.find((s) => s.id === id)).filter(Boolean)
+    })
 
-  function toggleStudent(student) {
-    const index = selectedStudentIds.value.findIndex((id) => id === student.id)
-    if (index > -1) {
-      selectedStudentIds.value.splice(index, 1)
-    } else {
-      selectedStudentIds.value.push(student.id)
+    function toggleStudent(student) {
+      const index = selectedStudentIds.value.findIndex((id) => id === student.id)
+      if (index > -1) {
+        selectedStudentIds.value.splice(index, 1)
+      } else {
+        selectedStudentIds.value.push(student.id)
+      }
     }
-  }
 
-  function resetSelection() {
-    selectedStudentIds.value = []
-  }
+    function resetSelection() {
+      selectedStudentIds.value = []
+    }
 
-  return {
-    studentsData,
-    selectedStudentIds,
-    selectedStudents,
-    toggleStudent,
-    resetSelection,
-  }
-}, { persist: true })
+    return {
+      studentsData,
+      selectedStudentIds,
+      selectedStudents,
+      toggleStudent,
+      resetSelection,
+    }
+  },
+  { persist: true }
+)
